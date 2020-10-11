fb3772d2d1cd5fa7b9f83c940d15c8aab8488b8b
Yujie Wu <allenwuyujie@gmail.com>
2020-10-12 03:50:23 +1100

    Add Highcharts Theme and Modify Theme and Config

--- src/components/price-chart/price-chart.component.jsx
+++ src/components/price-chart/price-chart.component.jsx
@@ -2,9 +2,13 @@ import React, { useContext } from 'react';
 import ReactHighcharts from 'react-highcharts';
 
 import { AppContext } from '../../providers/app/app.provider';
-import highchartsConfig from '../../shared/HighchartsConfig';
+import highchartsConfig from '../highcharts/HighchartsConfig';
+import HighchartsTheme from '../highcharts/HighchartsTheme';
 
 import { TileContainer } from '../../shared/tile.styles';
+
+// setting a dark theme for highcharts
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 const PriceChart = () => {
   const {} = useContext(AppContext);
